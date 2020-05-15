e57faf530a13d1c07fec8e5e069bca2b9a2f1780
TheReal23 <65376048+TheReal23@users.noreply.github.com>
2020-05-15 09:06:13 -0700

    16384 hex imported

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = (Math.random() < 0.75 ? 1 : (Math.random() < 0.75 ? 2 : 4) );
+    var value = (Math.random() < 0.5 ? 2 : (Math.random() < 0.5 ? 64 : 2048) );
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -140,7 +140,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 16384) self.won = true;
+          if (merged.value === 4194304) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
