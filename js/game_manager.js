4e6dbd7a25c92878fd43b453fe6c81951c30bc7e
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-09 16:17:26 -0500

    Create 65536 (hex) variant

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = (Math.random() < 0.5 ? 2 : (Math.random() < 0.5 ? 64 : 2048) );
+    var value = (Math.random() < 0.5 ? 1 : (Math.random() < 0.5 ? 2 : 4) );
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 4194304) self.won = true;
+          // The mighty 65536 tile
+          if (merged.value === 65536) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
